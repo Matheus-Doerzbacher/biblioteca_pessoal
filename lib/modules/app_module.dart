9c988b487cb9f8adf33969476cfcd1090a1e6cd0
Matheus Doerzbacher <matheus.doer@gmail.com>
2024-11-26 19:21:39 -0300

    Criado o icon e o splash

--- lib/modules/app_module.dart
+++ lib/modules/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:biblioteca_pessoal/modules/categoria/categoria_module.dart';
 import 'package:biblioteca_pessoal/modules/emprestimo/emprestimo_module.dart';
 import 'package:biblioteca_pessoal/modules/livro/livro_module.dart';
+import 'package:biblioteca_pessoal/modules/usuario/controllers/user_controller.dart';
 import 'package:biblioteca_pessoal/modules/usuario/usuario_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -14,10 +15,29 @@ class AppModule extends Module {
   @override
   void routes(RouteManager r) {
     r
-      ..module(Modular.initialRoute, module: LivroModule())
+      ..module(
+        Modular.initialRoute,
+        module: LivroModule(),
+        guards: [AuthGuard()],
+      )
       ..module('/login', module: UsuarioModule())
       ..module('/categoria', module: CategoriaModule())
       ..module('/emprestimo', module: EmprestimoModule());
     super.routes(r);
+  }
+}
+
+class AuthGuard extends RouteGuard {
+  AuthGuard() : super(redirectTo: '/login/');
+
+  @override
+  Future<bool> canActivate(String path, ModularRoute route) async {
+    // final controller = Modular.get<UserController>();
+
+    if (UserController.user == null) {
+      return false;
+    }
+
+    return true;
   }
 }
